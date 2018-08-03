ecd78c0c5480b3075c5ef38883a04f1ba7b3b6e9
IGoR Fonin <ifonin@dalee.ru>
2018-08-03 15:00:09 +0300

    es-lib

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ const LessAutoprefix = require('less-plugin-autoprefix');
 const autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 const merge = require('merge2');
 const del = require('del');
+const through = require('through2');
 
-const dist = path.join(__dirname, 'dist', 'es');
+const dist = path.join(__dirname, 'dist');
+const esPath = path.join(dist, 'es');
+const libPath = path.join(dist, 'lib');
 
 gulp.task('clean', () => {
     return del('dist');
@@ -20,36 +23,73 @@ gulp.task('less', () => {
             paths: [path.join(__dirname, 'src')],
             plugins: [autoprefix]
         }))
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(esPath))
+        .pipe(gulp.dest(libPath));
 });
 
 gulp.task('ts', () => {
     const result = gulp.src(['src/**/*.{tsx,ts}', './typings/*.d.ts'])
         .pipe(ts({
             rootDir: './src',
-            experimentalDecorators: true,
+            noUnusedParameters: true,
+            noUnusedLocals: true,
+            strictNullChecks: true,
+            target: 'es6',
             jsx: 'preserve',
             moduleResolution: 'node',
-            declaration: true
+            declaration: true,
+            allowSyntheticDefaultImports: true,
         }))
 
-    return merge([
-        result.dts.pipe(gulp.dest(dist)),
-        result.js.pipe(babel({
-            presets: [
-                "env",
-                "react"
-            ]
-        })).pipe(gulp.dest(dist))
-    ]);
+    return merge(
+        result.dts
+            .pipe(gulp.dest(esPath))
+            .pipe(gulp.dest(libPath)),
+        result.js
+            .pipe(babel({
+                presets: [
+                    '@babel/react'
+                ],
+                plugins: [
+                    require.resolve('@babel/plugin-transform-object-assign'),
+                    require.resolve('@babel/plugin-proposal-class-properties'),
+                    require.resolve('@babel/plugin-proposal-object-rest-spread'),
+                    require.resolve('@babel/plugin-transform-runtime'),
+                    require.resolve('@babel/plugin-transform-classes'),
+                    require.resolve('@babel/plugin-transform-block-scoping'),
+                ]
+            }))
+            .pipe(tranformLess())
+            .pipe(gulp.dest(esPath))
+            .pipe(babel({
+                presets: [
+                    '@babel/env'
+                ],
+                plugins: [
+                    require.resolve('babel-plugin-add-module-exports')
+                ]
+            }))
+
+            .pipe(gulp.dest(libPath))
+    );
 });
 
 gulp.task('pic', () => {
     return gulp.src('src/**/*.{png,svg}')
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(esPath))
+        .pipe(gulp.dest(libPath));
 });
 
 gulp.task('build', gulp.series(
     'clean',
     gulp.parallel('less', 'ts', 'pic')
 ));
+
+function tranformLess() {
+    return through.obj(function (file, encoding, next) {
+        const content = file.contents.toString(encoding);
+        file.contents = Buffer.from(content.replace(/\.less/g, '.css'));
+        this.push(file);
+        next();
+    });
+}
