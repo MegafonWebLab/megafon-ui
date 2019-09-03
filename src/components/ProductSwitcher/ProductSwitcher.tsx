6862fdfa161af3907b11cf29d1b74ab1ce4233b1
a.bashmakov <artur.bashmakov@shtormtech.ru>
2019-09-03 17:50:12 +0300

    6133 add swype to product switcher

--- src/components/ProductSwitcher/ProductSwitcher.tsx
+++ src/components/ProductSwitcher/ProductSwitcher.tsx
@@ -28,6 +28,13 @@ interface IProductSwitcherState {
     currentIndex: number;
 }
 
+interface INearPoint {
+    item: number;
+    coord: number;
+    value: string;
+    title: string;
+}
+
 const cn = cnCreate('mfui-product-switcher');
 class ProductSwitcher extends React.Component<IProductSwitcherProps, IProductSwitcherState> {
     static propTypes = {
@@ -46,6 +53,12 @@ class ProductSwitcher extends React.Component<IProductSwitcherProps, IProductSwi
     pointerNode: HTMLElement | null;
     colorRowNode: HTMLElement | null;
     timer: number;
+    rangeWrapperInfo: ClientRect | null;
+    rowItemsInfo: INearPoint[];
+    moveEvents = ['mousemove', 'touchmove'];
+    leaveEvents = ['mouseleave', 'mouseup', 'touchend'];
+    touchEvents = ['touchmove', 'touchend'];
+    switcherStep: number;
 
     constructor(props: IProductSwitcherProps) {
         super(props);
@@ -60,6 +73,8 @@ class ProductSwitcher extends React.Component<IProductSwitcherProps, IProductSwi
 
     componentDidMount() {
         window.addEventListener('resize', this.handleWindowSize, true);
+        this.getRangeWrapperCoords();
+        this.getRowItemsInfo();
         this.movePointer(this.state.currentValue);
     }
 
@@ -87,6 +102,9 @@ class ProductSwitcher extends React.Component<IProductSwitcherProps, IProductSwi
     }
 
     handleWindowSize = () => {
+        this.getRangeWrapperCoords();
+        this.getRowItemsInfo();
+
         clearTimeout(this.timer);
 
         this.timer = window.setTimeout(() => {
@@ -107,7 +125,166 @@ class ProductSwitcher extends React.Component<IProductSwitcherProps, IProductSwi
         this.movePointer(value);
     }
 
+    handleMouseDown = () => {
+        this.moveEvents.forEach(el => this.rootNode && this.rootNode.addEventListener(el, this.handleElementAction));
+        this.leaveEvents.forEach(el => this.rootNode && this.rootNode.addEventListener(el, this.handleElementAction));
+    }
+
+    handleElementAction = (e: Event | React.TouchEvent | React.MouseEvent) => {
+        e.preventDefault();
+
+        const eventName = e.type;
+        const isMoveEvent = this.moveEvents.some(el => el === eventName);
+
+        return isMoveEvent ?
+            this.handleMouseMove(this.touchEvents.some(el => el === eventName), e) :
+            this.handleMouseLeave(this.touchEvents.some(el => el === eventName), e);
+    }
+
+    handleMouseMove = (isTouchEvent: boolean, e) => {
+        const {
+            left: startPoint = 0,
+            right: endPoint = 0,
+            width = 0,
+        } = this.rangeWrapperInfo || {};
+        const point = this.pointerNode;
+        const colorRow = this.colorRowNode;
+        const pointHalfWidth = point && point.offsetWidth / 2 || 0;
+        const eventXCoord = isTouchEvent ? e.changedTouches[0].clientX : e.clientX;
+        const eventYCoord = isTouchEvent ? e.changedTouches[0].clientY : e.screenY;
+
+        if (!point) {
+            return;
+        }
+
+        if (isTouchEvent) {
+            this.getRangeWrapperCoords();
+
+            const {
+                left = 0,
+                right = 0,
+                top = 0,
+                bottom = 0,
+            } = this.rangeWrapperInfo || {};
+
+            if ((eventXCoord < left || eventXCoord > right) || (eventYCoord < top || eventYCoord > bottom)) {
+                this.handleMouseLeave(isTouchEvent, e);
+
+                return;
+            }
+        }
+
+        switch (true) {
+            case eventXCoord < startPoint + pointHalfWidth:
+                point.style.transform = 'translateX(0px)';
+                break;
+            case eventXCoord > endPoint - pointHalfWidth:
+                point.style.transform = `translateX(${width - pointHalfWidth}px)`;
+                break;
+            default:
+                point.style.transform = `translateX(${eventXCoord - startPoint - pointHalfWidth}px)`;
+        }
+
+        if (!colorRow) {
+            return;
+        }
+
+        colorRow.style.width = `${eventXCoord - startPoint}px`;
+    }
+
+    handleMouseLeave = (isTouchEvent: boolean, e) => {
+        const { left: startPoint = 0 } = this.rangeWrapperInfo || {};
+        const rowItemsInfo = this.rowItemsInfo;
+        const switcherStep = this.switcherStep;
+        const eventXCoord = isTouchEvent ? e.changedTouches[0].clientX : e.clientX;
+        const outRowPoint = eventXCoord - startPoint;
+        const nearPoint = rowItemsInfo.filter((el: INearPoint, ind: number, arr) => {
+            const prevEl = arr[ind - 1];
+
+            if (ind === 0) {
+                return outRowPoint <= switcherStep / 2;
+            }
+
+            if (ind === arr.length - 1) {
+                return outRowPoint >= prevEl.coord + switcherStep / 2;
+            }
+
+            return outRowPoint >= el.coord - switcherStep / 2 && outRowPoint <= el.coord + switcherStep / 2;
+        });
+
+        this.setState({ currentValue: nearPoint[0].value, currentIndex: nearPoint[0].item });
+        this.movePointer(nearPoint[0].value);
+
+        this.moveEvents.forEach(el => this.rootNode && this.rootNode.removeEventListener(el, this.handleElementAction));
+        this.leaveEvents.forEach(el =>
+            this.rootNode && this.rootNode.removeEventListener(el, this.handleElementAction)
+        );
+    }
+
+    getRangeWrapperCoords = () => {
+        this.rangeWrapperInfo = this.rootNode && this.rootNode.getBoundingClientRect();
+    }
+
+    getRowItemsInfo = () => {
+        const { items } = this.props;
+        const { width: rowWidth = 0 } = this.rangeWrapperInfo || {};
+        const itemsLength = items.length;
+
+        this.switcherStep = rowWidth / (itemsLength - 1);
+        this.rowItemsInfo = items.map((el, ind) => {
+            if (ind === 0) {
+                return {
+                    ...el,
+                    item: 0,
+                    coord: 0,
+                };
+            }
+
+            if (ind === itemsLength - 1) {
+                return {
+                    ...el,
+                    item: itemsLength - 1,
+                    coord: rowWidth,
+                };
+            }
+
+            return {
+                ...el,
+                item: ind,
+                coord: (rowWidth / (itemsLength - 1)) * ind,
+           };
+        });
+    }
+
     movePointer(value: string) {
+        const { theme } = this.props;
+
+        if (theme === 'with-color-row' && this.pointerNode && this.colorRowNode) {
+            const point = this.pointerNode;
+            const checkedItem = this.rowItemsInfo.filter((el: INearPoint ) => el.value === value);
+            let switchPointOffsetValue;
+            let switchRowffsetValue;
+
+            switch (true) {
+                case checkedItem[0].item === 0:
+                    switchPointOffsetValue = 0;
+                    switchRowffsetValue = 0;
+                    break;
+                case checkedItem[0].item === this.rowItemsInfo.length - 1:
+                    switchPointOffsetValue = checkedItem[0].coord - (point && point.offsetWidth);
+                    switchRowffsetValue = checkedItem[0].coord - (point && point.offsetWidth / 2);
+                    break;
+                default:
+                    switchPointOffsetValue = checkedItem[0].coord - (point && point.offsetWidth / 2);
+                    switchRowffsetValue = checkedItem[0].coord;
+            }
+
+            this.pointerNode.style.transform = `translateX(${switchPointOffsetValue}px)`;
+            this.colorRowNode.style.width = `${switchRowffsetValue}px`;
+
+            return;
+        }
+
         const rootOffsetLeft = this.rootNode ? this.rootNode.getBoundingClientRect().left : 0;
         const hasLabelProp = Object.keys(this.itemNodes).length !== 0;
         const targetLabelOffsetLeft = hasLabelProp ? this.itemNodes[value].getBoundingClientRect().left : 0;
@@ -161,7 +338,17 @@ class ProductSwitcher extends React.Component<IProductSwitcherProps, IProductSwi
                         </div>
                     )}
                 </div>
-                <div className={cn('pointer')} ref={this.getPointerNode}/>
+                {theme === 'with-color-row' ?
+                    <div
+                        className={cn('pointer')}
+                        ref={this.getPointerNode}
+                        onMouseDown={this.handleMouseDown}
+                        onTouchStart={this.handleMouseDown}
+                        onMouseUp={this.handleElementAction}
+                    />
+                    :
+                    <div className={cn('pointer')} ref={this.getPointerNode} />
+                }
                 <div className={cn('color-row')} ref={this.getColorRowNode}/>
             </div>
         );
