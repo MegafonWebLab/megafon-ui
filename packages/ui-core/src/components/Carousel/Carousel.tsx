95cdb2be8fbf482ce28d29164d7c35a37ae27074
Alexander Gaidarzhi <dalee@Dalees-Mac-mini.local>
2019-11-15 19:22:34 +0300

    fix lost position on resize

--- packages/ui-core/src/components/Carousel/Carousel.tsx
+++ packages/ui-core/src/components/Carousel/Carousel.tsx
@@ -82,6 +82,7 @@ class Carousel extends React.Component<ICarouselProps, ICarouselState> {
             isNextActive: true,
             isArrows: true,
             showSlides: 0,
+            scrollSlides: 0,
         };
     }
 
@@ -174,7 +175,11 @@ class Carousel extends React.Component<ICarouselProps, ICarouselState> {
         breakpoints.forEach((gap, index: number) => {
             const {
                 breakpoint,
-                settings: { slidesToShow, arrows },
+                settings: {
+                    slidesToShow,
+                    slidesToScroll,
+                    arrows
+                },
             } = gap;
             const nextIndex = index + 1;
             const isThisResolution = breakpoint >= windowWidth;
@@ -188,7 +193,11 @@ class Carousel extends React.Component<ICarouselProps, ICarouselState> {
             const isLast = nextIndex === breakpointsLength;
 
             if ((isNextBreakpoint && isThisResolution) || (isThisResolution && isLast)) {
-                responsiveData = { hasResponsiveArrows: isArrows, currentSlides: slidesToShowValue };
+                responsiveData = {
+                    slidesToScroll,
+                    hasResponsiveArrows: isArrows,
+                    currentSlides: slidesToShowValue
+                };
             }
         });
 
@@ -200,24 +209,68 @@ class Carousel extends React.Component<ICarouselProps, ICarouselState> {
             return;
         }
         const { innerSlider: { state: { currentSlide, slideCount } }, slickGoTo } = this.slider;
-        const { showSlides } = this.state;
+        const { showSlides = 0, scrollSlides = 0 } = this.state;
 
-        const lastVisibleSlideIndex: number = currentSlide + (showSlides - 1);
+        const numberOfSections = scrollSlides > 1
+            ? Math.ceil(slideCount / scrollSlides)
+            : Math.ceil(slideCount - (showSlides - scrollSlides));
 
-        if (lastVisibleSlideIndex >= slideCount) {
-            slickGoTo(slideCount - showSlides);
-        }
+
+        const slidesIndexes = new Array(slideCount).fill().map((item, i) => i);
+        const sectionLength = Math.ceil(showSlides);
+        const sliderSections = new Array(numberOfSections).fill(new Array(sectionLength));
+
+        const sliderSectionsWithSlides = sliderSections.map((section, i) => {
+            const startIndex = i * scrollSlides;
+            return slidesIndexes.slice(startIndex, startIndex + sectionLength);
+        });
+
+        console.log(sliderSectionsWithSlides);
+
+        // let i = 0;
+        // const data = new Array(slideCount).fill().map(() => i++);
+        // const structureScheme = new Array(Math.ceil(numberOfSections)).fill(new Array(Math.ceil(showSlides)));
+        //
+        // const structureSchemeWithData = structureScheme.map((arr, i) => {
+        //     const startIndex = i * scrollSlides;
+        //     return data.slice(startIndex, startIndex + arr.length);
+        // });
+        //
+        // let section;
+        // for (let internalArr of structureSchemeWithData.reverse()) {
+        //     if (internalArr.includes(currentSlide)) {
+        //         section = internalArr;
+        //         break;
+        //     }
+        // }
+        //
+        // if (!section) {
+        //     return;
+        // }
+        //
+        // const rightSlide = section[0];
+        //
+        // if (currentSlide === rightSlide) {
+        //     return;
+        // }
+        //
+        // slickGoTo(rightSlide);
     }
 
     handleCarouselParams = () => {
         const {
             children,
-            options: { slidesToShow, responsive, arrows = true },
+            options: {
+                slidesToShow,
+                slidesToScroll: nonResponsiveSlidesToScroll,
+                responsive,
+                arrows = true
+            },
         } = this.props;
         const slider = this.slider || { innerSlider: { state: {} } };
         const { innerSlider: { state: { currentSlide } } } = slider;
         const childsAmount = children.length;
-        const { hasResponsiveArrows, currentSlides } = this.getResponsiveData(
+        const { hasResponsiveArrows, currentSlides, slidesToScroll } = this.getResponsiveData(
             responsive,
             arrows,
             slidesToShow,
@@ -227,9 +280,10 @@ class Carousel extends React.Component<ICarouselProps, ICarouselState> {
             hasResponsiveArrows :
             arrows && slidesToShow < childsAmount;
         const showSlides = currentSlides || slidesToShow;
+        const scrollSlides = slidesToScroll || nonResponsiveSlidesToScroll;
 
         this.setState(
-            { isArrows, showSlides },
+            { isArrows, showSlides, scrollSlides },
             (): void => {
                 this.updateArrowsState(currentSlide);
             }
@@ -265,6 +319,8 @@ class Carousel extends React.Component<ICarouselProps, ICarouselState> {
         const { className, options, theme, children } = this.props;
         const { isArrows } = this.state;
         const { arrows, ...carouselOptions } = options;
+        window.slider = this.slider;
+        this.slider && console.log(this.slider.innerSlider.state.currentSlide);
 
         return (
             <div className={cn('', {theme}, className)}>
