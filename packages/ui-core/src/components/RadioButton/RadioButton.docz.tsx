aaa8613d2cdfbeffc92a09f6a616c29a580299b1
Aleksandra Misyukevich <a.misyukevich@shtormtech.ru>
2020-06-02 13:00:33 +0300

    MFRU-7542 fix notes

--- packages/ui-core/src/components/RadioButton/RadioButton.docz.tsx
+++ packages/ui-core/src/components/RadioButton/RadioButton.docz.tsx
@@ -6,10 +6,6 @@ export const flexStyle = {
     justifyContent: 'space-around',
 };
 
-const textPosition = {
-    textAlign: 'center',
-} as React.CSSProperties;
-
 export interface IRadioButtonProps {
     children: (prop: { selectedOption?: string; onChange: (value: string) => void }) => JSX.Element;
 }
@@ -18,27 +14,22 @@ export interface IRadioButtonState {
     selectedOption?: string;
 }
 
-export class DemoRadioButtonWrapper extends React.Component<IRadioButtonProps, IRadioButtonState> {
-    constructor(props: IRadioButtonProps) {
-        super(props);
-        this.state = {
-            selectedOption: '',
-        };
-    }
-    handleChange = (value: string): void => {
-        this.setState({ selectedOption: value });
-    }
-    render() {
-        const { selectedOption } = this.state;
-        const { children } = this.props;
-        return (
-            <>
-                <p style={textPosition}><b>Value:</b> "{selectedOption || undefined}"</p><br />
+export const DemoRadioButtonWrapper = ({ children }: IRadioButtonProps) => {
+    const [selectedOption, setSelectedOption] = React.useState('');
+
+    const handleChange = (value: string): void => {
+        setSelectedOption(value);
+    };
+
+    return (
+        <>
+            <p style={{textAlign: 'center'}}><b>Value:</b> "{selectedOption || undefined}"</p>
+            <div style={flexStyle}>
                 {children({
-                    onChange: this.handleChange,
+                    onChange: handleChange,
                     selectedOption: selectedOption,
                 })}
-            </>
-        );
-    }
-}
+            </div>
+        </>
+    );
+};
