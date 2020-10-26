96a0a38001f91b0a6ae42e3f3964065beaddd11f
denis.fomin <denis.fomin@megafon.ru>
2020-10-26 21:51:17 +0300

    fix(dropdownsociallist): replaced BubbleHint with Tooltip

--- packages/ui-core/src/components/DropdownSocialList/DropdownSocialList.tsx
+++ packages/ui-core/src/components/DropdownSocialList/DropdownSocialList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import BubbleHint from '../BubbleHint/BubbleHint';
+import Tooltip from 'components/Tooltip/Tooltip';
 import './DropdownSocialList.less';
 import cnCreate from 'utils/cnCreate';
 
@@ -13,73 +13,59 @@ export interface IDropdownSocialListProps {
     /** Icons list */
     icons: Array<Partial<IIcon>>;
     /** Max icon */
-    maxIconNumber: number;
+    maxIconNumber?: number;
     /** Custom class name */
     className?: string;
 }
 
 const cn = cnCreate('mfui-beta-dropdown-social-list');
-class DropdownSocialList extends React.Component<IDropdownSocialListProps, {}> {
-    static propTypes = {
-        icons: PropTypes.arrayOf(
-            PropTypes.shape({
-                svgIcon: PropTypes.element,
-                title: PropTypes.string,
-            })
-        ),
-        maxIconNumber: PropTypes.number,
-        className: PropTypes.string,
-    };
+const DropdownSocialList: React.FC<IDropdownSocialListProps> = ({icons, maxIconNumber = 6, className}) => {
+    const [ triggerElement, setTriggerElement ] = React.useState<HTMLSpanElement | null>(null);
 
-    static defaultProps: Partial<IDropdownSocialListProps> = {
-        maxIconNumber: 6,
-    };
-
-    renderDropdown() {
-        const { icons, maxIconNumber }: IDropdownSocialListProps = this.props;
-
-        return (
-            <div>{/* div for ios bug, don't delete */}
-                <BubbleHint
-                    className={cn('dropdown')}
-                    click
-                    placement="bottom"
-                    popupPadding="none"
-                    trigger={
-                        <span className={cn('dropdown-trigger')}>
-                            и еще {icons.length - maxIconNumber}
-                        </span>}
-                >
-                    <div className={cn('dropdown-popup')}>
-                        <div className={cn('dropdown-list')}>
-                            {icons.slice(maxIconNumber).map((icon: IIcon, index: number): JSX.Element =>
-                                <span className={cn('dropdown-item')} key={icon.title + index}>
-                                    {icon.title}
-                                </span>
-                            )}
-                        </div>
-                    </div>
-                </BubbleHint>
-            </div>
-        );
-    }
-
-    render() {
-        const { icons, maxIconNumber, className }: IDropdownSocialListProps = this.props;
-
-        return (
-            <div className={cn('', {}, className)}>
-                <div className={cn('list')}>
-                    {icons.slice(0, maxIconNumber).map((icon: IIcon, index): JSX.Element =>
-                        <div className={cn('item')} title={icon.title} key={icon.title + index}>
-                            {icon.svgIcon}
-                        </div>
+    const renderDropdown = (): React.ReactNode => (
+        <div className={cn('dropdown')}>
+            <span ref={setTriggerElement} className={cn('dropdown-trigger')}>
+                и еще {icons.length - maxIconNumber}
+            </span>
+            <Tooltip
+                placement="bottom"
+                triggerEvent="click"
+                triggerElement={triggerElement}
+            >
+                <div className={cn('dropdown-list')}>
+                    {icons.slice(maxIconNumber).map((icon: IIcon, index: number): JSX.Element =>
+                        <span className={cn('dropdown-item')} key={icon.title + index}>
+                            {icon.title}
+                        </span>
                     )}
                 </div>
-                {icons.length > maxIconNumber && this.renderDropdown()}
+            </Tooltip>
+        </div>
+    );
+
+    return (
+        <div className={cn([className])}>
+            <div className={cn('list')}>
+                {icons.slice(0, maxIconNumber).map((icon: IIcon, index): JSX.Element =>
+                    <div className={cn('item')} title={icon.title} key={icon.title + index}>
+                        {icon.svgIcon}
+                    </div>
+                )}
             </div>
-        );
-    }
-}
+            {icons.length > maxIconNumber && renderDropdown()}
+        </div>
+    );
+};
+
+DropdownSocialList.propTypes = {
+    icons: PropTypes.arrayOf(
+        PropTypes.shape({
+            svgIcon: PropTypes.element.isRequired,
+            title: PropTypes.string.isRequired,
+        }).isRequired
+    ).isRequired,
+    maxIconNumber: PropTypes.number,
+    className: PropTypes.string,
+};
 
 export default DropdownSocialList;
