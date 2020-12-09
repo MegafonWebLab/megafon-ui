32d453d4cbc49cf5309a453ebed030123e69078a
astranavt <astranomnom@gmail.com>
2020-12-09 12:35:14 +0300

    refactor(component): unused variable remove

--- packages/ui-shared/src/components/BannerBox/BannerBox.tsx
+++ packages/ui-shared/src/components/BannerBox/BannerBox.tsx
@@ -1,12 +1,6 @@
 import * as React from 'react';
 import { cnCreate, Banner } from '@megafon/ui-core';
 
-export const NavTheme = {
-    LIGHT: 'light',
-    GREEN: 'green',
-    DARK: 'dark',
-} as const;
-
 type BannerBoxProps = React.ComponentProps<typeof Banner>;
 
 const cn = cnCreate('mfui-beta-banner-box');
