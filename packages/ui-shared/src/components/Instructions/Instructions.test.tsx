c79ab2734f2cefef6386ffe9ccd306af9f1a986e
a.bashmakov <artur.bashmakov@shtormtech.ru>
2020-12-10 17:24:51 +0300

    feat(instructions): add component

---
+++ packages/ui-shared/src/components/Instructions/Instructions.test.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import { shallow, mount } from 'enzyme';
+import { act } from 'react-dom/test-utils';
+import Instructions, {
+    pictureMaskTypes,
+    pictureAlignTypes,
+    IInstructionsProps,
+    InstructionItemType
+} from './Instructions';
+
+type LocalWindowType = Omit<Window, 'innerWidth'> & {
+    innerWidth: number;
+};
+
+const items: InstructionItemType[] = [
+    {
+        title: 'Test1',
+        mediaUrl: 'ImgUrl1',
+        isVideo: true,
+    },
+    {
+        title: 'Test2',
+        mediaUrl: 'ImgUrl2',
+        isVideo: false,
+    },
+];
+
+const props: IInstructionsProps = {
+    title: 'Test instruction title',
+    instructionItems: items,
+};
+
+describe('<Instructions />', () => {
+    describe('desktop resolution', () => {
+        it('should render with default props', () => {
+            const wrapper = shallow(
+                <Instructions {...props} />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('should render with right side picture', () => {
+            const newProps = {
+                ...props,
+                pictureAlign: pictureAlignTypes.RIGHT,
+            };
+
+            const wrapper = shallow(
+                <Instructions {...newProps} />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('should render with pictureMask', () => {
+            const newProps = {
+                ...props,
+                pictureMask: pictureMaskTypes.BLACK_IPHONE,
+            };
+
+            const wrapper = shallow(
+                <Instructions {...newProps} />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('active slide will be changed', () => {
+            const wrapper = shallow(
+                <Instructions {...props} />
+            );
+
+            wrapper.find('.mfui-beta-instructions__articles-item').at(1).simulate('click');
+
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+
+    describe('mobile resolution', () => {
+        const localWindow = window as LocalWindowType;
+        const windowInnerWidth = window.innerWidth;
+
+        localWindow.innerWidth = 1023;
+
+        jest.spyOn(Date, 'now').mockImplementation(() => 1);
+
+        afterAll(() => {
+            jest.restoreAllMocks();
+            localWindow.innerWidth = windowInnerWidth;
+        });
+
+        it('should render with default props', () => {
+            const wrapper = mount(
+                <Instructions {...props} />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('should rerender when window resize', () => {
+            const wrapper = mount(
+                <Instructions {...props} />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+
+            localWindow.innerWidth = 1280;
+
+            act(() => {
+                window.dispatchEvent(new Event('resize'));
+            });
+
+            wrapper.update();
+            expect(wrapper).toMatchSnapshot();
+        });
+    });
+});
---
+++ packages/ui-shared/src/components/Instructions/Instructions.tsx
@@ -0,0 +1,210 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import './Instructions.less';
+import throttle from 'lodash.throttle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperClass from 'swiper/types/swiper-class';
+import { breakpoints, cnCreate, Grid, GridColumn, Header, Paragraph } from '@megafon/ui-core';
+
+export const pictureAlignTypes = {
+    LEFT: 'left',
+    RIGHT: 'right',
+} as const;
+
+export const pictureMaskTypes = {
+    ANDROID: 'android',
+    NEW_IPHONE: 'new-iphone',
+    BLACK_IPHONE: 'black-iphone',
+    WHITE_IPHONE: 'white-iphone',
+    LAPTOP: 'laptop',
+    NONE: 'none',
+} as const;
+
+type PictureAlignTypesType = typeof pictureAlignTypes[keyof typeof pictureAlignTypes];
+
+type PictureMaskTypesType = typeof pictureMaskTypes[keyof typeof pictureMaskTypes];
+
+export type InstructionItemType = {
+    title: string;
+    mediaUrl: string;
+    isVideo: boolean;
+};
+
+export interface IInstructionsProps {
+    /** Заголовок инструкции */
+    title: string;
+    /** Пункты инструкции */
+    instructionItems: InstructionItemType[];
+    /** Расположение изображения */
+    pictureAlign?: PictureAlignTypesType;
+    /** Маска изображения */
+    pictureMask?: PictureMaskTypesType;
+}
+
+const cn = cnCreate('mfui-beta-instructions');
+const swiperSlideCn = cn('slide');
+const Instructions: React.FC<IInstructionsProps> = ({
+    title,
+    instructionItems,
+    pictureAlign = 'left',
+    pictureMask = 'none',
+}) => {
+    const [swiperInstance, setSwiperInstance] = React.useState<SwiperClass>();
+    const [slideIndex, setSlideIndex] = React.useState(0);
+    const [isMobile, setIsMobile] = React.useState(false);
+
+    const getSwiperInstance = React.useCallback((swiper: SwiperClass): void => {
+        setSwiperInstance(swiper);
+    }, []);
+
+    const handleResize = React.useCallback((): void => {
+        const isMobileScreen = window.innerWidth < breakpoints.desktopSmallStart;
+
+        setIsMobile(isMobileScreen);
+    }, []);
+
+    const handleArticleClick = React.useCallback((ind: number) => (): void => {
+        setSlideIndex(ind);
+        swiperInstance && swiperInstance.slideTo(ind);
+    }, [swiperInstance]);
+
+    const renderTitle = React.useCallback((resolution: string): JSX.Element => (
+        <Header className={cn('title', { resolution })} as="h2">
+            {title}
+        </Header>
+    ), []);
+
+    const renderPicture = React.useCallback((): JSX.Element => {
+        if (pictureMask === pictureMaskTypes.NONE) {
+            return renderSlider();
+        }
+
+        return (
+            <div className={cn('img-wrapper')}>
+                <div className={cn('device-screen')} />
+                {renderSlider()}
+            </div>
+        );
+    }, [pictureMask]);
+
+    const renderVideo = React.useCallback((mediaUrl: string): JSX.Element => (
+        <video className={cn('swiper-img')} autoPlay muted loop>
+            <source src={mediaUrl} type="video/mp4" />
+        </video>
+    ), []);
+
+    const renderSlider = React.useCallback((): JSX.Element => (
+        <Swiper
+            className={cn('swiper')}
+            onSwiper={getSwiperInstance}
+            noSwipingClass={swiperSlideCn}
+        >
+            {instructionItems.map(({ mediaUrl, isVideo}, ind) => (
+                <SwiperSlide className={swiperSlideCn} key={ind + mediaUrl}>
+                    {isVideo
+                        ? renderVideo(mediaUrl)
+                        : <img className={cn('swiper-img')} src={mediaUrl} alt="" />
+                    }
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    ), [instructionItems]);
+
+    const renderDesktopArticles = React.useCallback((): JSX.Element => (
+        <ul className={cn('articles-list')}>
+            {instructionItems.map(({title: itemTitle}, ind) => (
+                <li
+                    className={cn('articles-item', {active: slideIndex === ind})}
+                    onClick={handleArticleClick(ind)}
+                    key={ind}
+                >
+                    <div className={cn('articles-item-dot')}>
+                        <span className={cn('articles-item-dot-number')}>{ind + 1}</span>
+                    </div>
+                    <Paragraph className={cn('articles-item-title')} hasMargin={false}>
+                        {itemTitle}
+                    </Paragraph>
+                </li>
+            ))}
+        </ul>
+    ), [instructionItems, slideIndex, handleArticleClick]);
+
+    const renderMobileArticles = React.useCallback((): JSX.Element => (
+        <>
+            <div className={cn('articles-title-block')}>
+                {instructionItems.map(({ title: itemTitle }, ind) => (
+                    slideIndex === ind &&
+                        <Paragraph className={cn('articles-title')} hasMargin={false} key={ind}>
+                            {itemTitle}
+                        </Paragraph>
+                ))}
+            </div>
+            <ul className={cn('articles-dots')}>
+                {instructionItems.map((_item, ind) => (
+                    <div
+                        key={ind}
+                        className={cn('articles-dot', {active: slideIndex === ind})}
+                        onClick={handleArticleClick(ind)}
+                    >
+                        <span className={cn('articles-dot-number')}>{ind + 1}</span>
+                    </div>
+                ))}
+            </ul>
+        </>
+    ), [instructionItems, slideIndex, handleArticleClick]);
+
+    React.useEffect(() => {
+        const handleSetThrottled = throttle(handleResize, 100);
+
+        handleResize();
+        window.addEventListener('resize', handleSetThrottled);
+
+        return (): void => {
+            window.removeEventListener('resize', handleSetThrottled);
+        };
+    }, [handleResize]);
+
+    return (
+        <div className={cn({ mask: pictureMask })}>
+            <Grid hAlign="center">
+                <GridColumn all="12" >
+                    {renderTitle('mobile')}
+                    <div className={cn('wrapper')}>
+                        <div className={cn('picture', { align: pictureAlign })}>
+                            {renderPicture()}
+                        </div>
+                        <div className={cn('articles', { align: pictureAlign })}>
+                            {renderTitle('desktop')}
+                            {isMobile
+                                ? renderMobileArticles()
+                                : renderDesktopArticles()
+                            }
+                        </div>
+                    </div>
+                </GridColumn>
+            </Grid>
+        </div>
+    );
+};
+
+Instructions.propTypes = {
+    title: PropTypes.string.isRequired,
+    instructionItems: PropTypes.arrayOf(
+        PropTypes.shape({
+            title: PropTypes.string.isRequired,
+            mediaUrl: PropTypes.string.isRequired,
+            isVideo: PropTypes.bool.isRequired,
+        }).isRequired
+    ).isRequired,
+    pictureAlign: PropTypes.oneOf([pictureAlignTypes.LEFT, pictureAlignTypes.RIGHT]),
+    pictureMask: PropTypes.oneOf([
+        pictureMaskTypes.ANDROID,
+        pictureMaskTypes.NEW_IPHONE,
+        pictureMaskTypes.WHITE_IPHONE,
+        pictureMaskTypes.BLACK_IPHONE,
+        pictureMaskTypes.LAPTOP,
+        pictureMaskTypes.NONE,
+    ]),
+};
+
+export default Instructions;
