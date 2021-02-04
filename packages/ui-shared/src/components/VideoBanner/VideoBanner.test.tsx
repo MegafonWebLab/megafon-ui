a0799ff06f8337d2f4d2669153aadeb6ee88e424
Alexander <vinokurov.ag@mail.ru>
2021-02-04 10:12:36 +0300

    feat(videobanner): added content text color prop, added image props for all resolutions
    BREAKING CHANGE: Removed image prop as an object. Added separate image props for each resolution.

--- packages/ui-shared/src/components/VideoBanner/VideoBanner.test.tsx
+++ packages/ui-shared/src/components/VideoBanner/VideoBanner.test.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import { shallow, mount } from 'enzyme';
 import { cnCreate } from '@megafon/ui-core';
-import VideoBanner, { VideoTypes } from './VideoBanner';
+import VideoBanner, { VideoType, TextColor, ClassName } from './VideoBanner';
 
-const onlyMobileImage = {
-    mobile: 'image-mobile.png',
-};
-
-const image = {
-    ...onlyMobileImage,
-    desktop: 'image-desktop.png',
-};
+const imageMobile = 'imageMobile';
+const imageTablet = 'imageTablet';
+const imageDesktop = 'imageDesktop';
+const imageDesktopWide = 'imageDesktopWide';
 
 const video = 'video.mp4';
-
 const youtubeVideoId = '2Sps5MnvlKM';
 
 const content = {
@@ -21,53 +16,32 @@ const content = {
     description: 'Описание должно быть примерно не более 130 символов. Пишите содержательно, кратно и не будет проблем с текстовым контентом.',
     href: '#',
     buttonTitle: 'Текст в кнопке',
+    textColor: TextColor.CLEAR_WHITE,
+};
+
+type LocalWindowType = Omit<Window, 'innerWidth'> & {
+    innerWidth: number;
 };
 
 const cn = cnCreate('mfui-beta-video-banner');
 describe('<VideoBanner />', () => {
-    it('render component on mobile resolution only with required props', () => {
-        const component = shallow(<VideoBanner image={onlyMobileImage} />);
-        expect(component).toMatchSnapshot();
-    });
-
-    it('render component on desktop resolution only with required props', () => {
-        const component = mount(<VideoBanner image={image} />);
-        expect(component).toMatchSnapshot();
-    });
-
-    it('render with content', () => {
-        const component = mount(<VideoBanner image={image} content={content} />);
-        expect(component).toMatchSnapshot();
-    });
-
-    it('render with youtube video', () => {
-        const component = mount(
-            <VideoBanner image={onlyMobileImage} videoType={VideoTypes.YOUTUBE} videoSrc={youtubeVideoId} />
-            );
-        expect(component).toMatchSnapshot();
-    });
-
-    it('render with video', () => {
-        // Props isMuted must be false, if the rendering method is "mount". Otherwise an error will occur.
-        // https://github.com/enzymejs/enzyme/issues/2326
-        const component = mount(
-            <VideoBanner image={onlyMobileImage} videoType={VideoTypes.VIDEO} videoSrc={video} isMuted={false} />
-            );
-        expect(component).toMatchSnapshot();
-    });
-
-    it('render with video and content', () => {
-        // Props isMuted must be false, if the rendering method is "mount". Otherwise an error will occur.
-        // https://github.com/enzymejs/enzyme/issues/2326
-        const component = mount(
+    it('render component with pictures', () => {
+        const wrapper = shallow(
             <VideoBanner
-                image={onlyMobileImage}
-                videoType={VideoTypes.VIDEO}
-                videoSrc={video}
-                content={content}
-                isMuted={false} />
-                );
-        expect(component).toMatchSnapshot();
+                imageMobile={imageMobile}
+                imageTablet={imageTablet}
+                imageDesktop={imageDesktop}
+                imageDesktopWide={imageDesktopWide}
+            />
+        );
+
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it('render component with pictures and content', () => {
+        const wrapper = shallow(<VideoBanner imageMobile={imageMobile} imageTablet={imageTablet} content={content} />);
+
+        expect(wrapper).toMatchSnapshot();
     });
 
     it('should call onClick props', () => {
@@ -77,10 +51,160 @@ describe('<VideoBanner />', () => {
             onButtonClick,
         };
 
-        const component = shallow(<VideoBanner image={image} content={contentWithMockFunc} />);
-        const btn = component.find(`.${cn('button')}`);
+        const component = shallow(
+            <VideoBanner imageMobile={imageMobile} imageTablet={imageTablet} content={contentWithMockFunc} />);
+        const btn = component.find(`.${cn(ClassName.BUTTON)}`);
 
         btn.simulate('click');
         expect(onButtonClick).toBeCalled();
+    });
+
+    describe('tests with local window', () => {
+        let localWindow;
+        const windowInnerWidth = window.innerWidth;
+
+        beforeEach(() => {
+            localWindow = window as LocalWindowType;
+        });
+
+        afterEach(() => {
+            localWindow.innerWidth = windowInnerWidth;
+        });
+
+        it('render with youtube video', () => {
+            localWindow.innerWidth = 1920;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    videoType={VideoType.YOUTUBE}
+                    videoSrc={youtubeVideoId}
+                />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('render with video', () => {
+            localWindow.innerWidth = 1920;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    videoType={VideoType.VIDEO}
+                    videoSrc={video}
+                />
+            );
+
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('render with video and content', () => {
+            localWindow.innerWidth = 1920;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    videoType={VideoType.VIDEO}
+                    videoSrc={video}
+                    content={content}
+                />
+            );
+            expect(wrapper).toMatchSnapshot();
+        });
+
+        it('should not render video on mobile resolution', () => {
+            localWindow.innerWidth = 320;
+            const wrapper = shallow(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageDesktop}
+                    videoType={VideoType.VIDEO}
+                    videoSrc={video}
+                />
+            );
+            const videoElement = wrapper.find(`.${cn(ClassName.VIDEO)}`);
+
+            expect(videoElement).toHaveLength(0);
+        });
+
+        it('should not render background image on desktop resolution with video', () => {
+            localWindow.innerWidth = 1920;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    videoType={VideoType.YOUTUBE}
+                    videoSrc={youtubeVideoId}
+                />
+            );
+            const backgroundImageElement = wrapper.find(`.${cn(ClassName.BACKGROUND_IMAGE)}`);
+
+            expect(backgroundImageElement).toHaveLength(0);
+        });
+
+        it('render mobile image on relevant resolution', () => {
+            localWindow.innerWidth = 320;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    imageDesktop={imageDesktop}
+                    imageDesktopWide={imageDesktopWide}
+                />
+            );
+            const backgroundImageElement = wrapper.find(`.${cn(ClassName.BACKGROUND_IMAGE)}`);
+            const imageSource = backgroundImageElement.get(0).props.style.backgroundImage;
+
+            expect(imageSource).toBe(`url(${imageMobile})`);
+        });
+
+        it('render tablet image on relevant resolution', () => {
+            localWindow.innerWidth = 768;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    imageDesktop={imageDesktop}
+                    imageDesktopWide={imageDesktopWide}
+                />
+            );
+            const backgroundImageElement = wrapper.find(`.${cn(ClassName.BACKGROUND_IMAGE)}`);
+            const imageSource = backgroundImageElement.get(0).props.style.backgroundImage;
+
+            expect(imageSource).toBe(`url(${imageTablet})`);
+        });
+
+        it('render desktop image on relevant resolution', () => {
+            localWindow.innerWidth = 1024;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    imageDesktop={imageDesktop}
+                    imageDesktopWide={imageDesktopWide}
+                />
+            );
+            const backgroundImageElement = wrapper.find(`.${cn(ClassName.BACKGROUND_IMAGE)}`);
+            const imageSource = backgroundImageElement.get(0).props.style.backgroundImage;
+
+            expect(imageSource).toBe(`url(${imageDesktop})`);
+        });
+
+        it('render desktop image on relevant resolution', () => {
+            localWindow.innerWidth = 1280;
+            const wrapper = mount(
+                <VideoBanner
+                    imageMobile={imageMobile}
+                    imageTablet={imageTablet}
+                    imageDesktop={imageDesktop}
+                    imageDesktopWide={imageDesktopWide}
+                />
+            );
+            const backgroundImageElement = wrapper.find(`.${cn(ClassName.BACKGROUND_IMAGE)}`);
+            const imageSource = backgroundImageElement.get(0).props.style.backgroundImage;
+
+            expect(imageSource).toBe(`url(${imageDesktopWide})`);
+        });
     });
 });
