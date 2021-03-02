3b3686ae1304947f873b95d735f9269741ccb7f5
Nikolay Ilin <nikolay.ilin@shtormtech.ru>
2021-03-02 16:20:16 +0300

    feat(titledescriptionbox): add dataAttrs prop

--- packages/ui-shared/src/components/TitleDescriptionBox/TitleDescriptionBox.tsx
+++ packages/ui-shared/src/components/TitleDescriptionBox/TitleDescriptionBox.tsx
@@ -5,11 +5,14 @@ import {
     Header,
     Paragraph,
     Grid,
-    GridColumn
+    GridColumn,
+    dataAttrs as filterDataAttrs,
 } from '@megafon/ui-core';
 import './TitleDescriptionBox.less';
 
 export interface ITitleDescriptionBoxProps {
+    /** Дата атрибуты для корневого элемента */
+    dataAttrs?: { [key: string]: string };
     /** Дополнительный класс корневого элемента */
     className?: string;
     /** Заголовок */
@@ -24,6 +27,7 @@ export interface ITitleDescriptionBoxProps {
 
 const cn = cnCreate('mfui-beta-title-description-box');
 const TitleDescriptionBox: React.FC<ITitleDescriptionBoxProps> = ({
+    dataAttrs,
     title,
     description,
     textColor,
@@ -31,6 +35,7 @@ const TitleDescriptionBox: React.FC<ITitleDescriptionBoxProps> = ({
     className,
 }) => (
     <div
+        {...filterDataAttrs(dataAttrs)}
         className={cn(
             { 'h-align': hAlign, 'text-color': textColor },
             className
@@ -62,6 +67,7 @@ const TitleDescriptionBox: React.FC<ITitleDescriptionBoxProps> = ({
 );
 
 TitleDescriptionBox.propTypes = {
+    dataAttrs: PropTypes.objectOf(PropTypes.string.isRequired),
     className: PropTypes.string,
     title: PropTypes.string,
     description: PropTypes.string,
