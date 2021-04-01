5c51813f5a04cef4047608d85b1e69d9ede73b59
Alexander <vinokurov.ag@mail.ru>
2021-04-01 16:19:20 +0300

    feat(steps): delete convert from component
    BREAKING CHANGE: Component does not support html in text property

--- packages/ui-shared/src/components/Steps/StepsItem.test.tsx
+++ packages/ui-shared/src/components/Steps/StepsItem.test.tsx
@@ -5,7 +5,7 @@ import StepsItem from './StepsItem';
 describe('StepsItem', () => {
     it('should render component', () => {
         const wrapper = shallow(
-            <StepsItem index={1} text="text <a href=# target=_self>link</a> <b>bold</b>" />);
+            <StepsItem index={1} text="text" />);
 
         expect(wrapper).toMatchSnapshot();
     });
