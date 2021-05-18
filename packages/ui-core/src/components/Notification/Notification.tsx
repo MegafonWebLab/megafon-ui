8b5783a4b97b9c077219fac7c851e52f105ea5d1
dmitriy.lobasov <dmitriy.lobasov@shtormtech.ru>
2021-05-18 11:50:31 +0300

    refactor(notification): fixes in demo and css code style

--- packages/ui-core/src/components/Notification/Notification.tsx
+++ packages/ui-core/src/components/Notification/Notification.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as PropTypes from 'prop-types';
 import './Notification.less';
 import cnCreate from 'utils/cnCreate';
 import Tile from 'components/Tile/Tile';
@@ -7,15 +8,6 @@ import TextLink from 'components/TextLink/TextLink';
 import CancelIcon from 'icons/System/32/Cancel_32.svg';
 import RightArrow from 'icons/System/16/Arrow_right_16.svg';
 
-export const LinkTargetTypes = {
-    SUCCESS: '_self',
-    WARNING: '_blank',
-    ERROR: '_parent',
-    INFO: '_top',
-} as const;
-
-type LinkTargetType = typeof LinkTargetTypes[keyof typeof LinkTargetTypes];
-
 export const NotificationTypes = {
     SUCCESS: 'success',
     WARNING: 'warning',
@@ -23,7 +15,7 @@ export const NotificationTypes = {
     INFO: 'info',
 } as const;
 
-type NotificationTypesType = typeof NotificationTypes[keyof typeof NotificationTypes];
+type NotificationType = typeof NotificationTypes[keyof typeof NotificationTypes];
 
 export const ShadowTypes = {
     ZERO: 'zero',
@@ -38,25 +30,25 @@ export interface INotificationProps {
     /** Дополнительный класс корневого элемента */
     className?: string;
     /** Тип отображения */
-    type?: NotificationTypesType;
+    type?: NotificationType;
     /** Уровень тени */
     shadowLevel?: ShadowType;
-    /** Наличие фоновой заливки у нотификации */
+    /** Наличие фоновой заливки у уведомления */
     isColored?: boolean;
     /** Наличие кнопки-крестика "Закрыть" */
     hasCloseButton?: boolean;
     /** Заголовок */
     title?: string;
-    /** Контент/текст нотификации */
+    /** Контент/текст уведомления */
     content?: string;
-    /** Текст ссылки внизу нотификации */
+    /** Текст ссылки внизу уведомления */
     link?: string;
     /** rel - аргумент тега <a> для ссылки */
     rel?: string;
     /** href - аргумент тега <a> для ссылки */
     href?: string;
     /** target - аргумент тега <a> */
-    target?: LinkTargetType;
+    target?: '_self' | '_blank' | '_parent' | '_top';
     /** Иконка */
     icon: JSX.Element;
     /** Обработчик на закрытие */
@@ -68,8 +60,8 @@ export interface INotificationProps {
 const cn = cnCreate('mfui-beta-notification');
 const Notification: React.FC<INotificationProps> = ({
     className,
-    type = NotificationTypes.SUCCESS,
-    shadowLevel = ShadowTypes.ZERO,
+    type = 'success',
+    shadowLevel = 'zero',
     isColored = false,
     hasCloseButton,
     title,
@@ -102,20 +94,40 @@ const Notification: React.FC<INotificationProps> = ({
                 </div>
 
                 <div className={cn('content')}>
-                    {title && <Header as="h5" className={cn('title')}>
-                        {title}
-                    </Header>}
+                    {title && (
+                        <Header as="h5" className={cn('title')}>
+                            {title}
+                        </Header>
+                    )}
                     {content &&
                         <p className={cn('text')}>{content}</p>
                     }
                     {link && renderLink()}
                 </div>
             </div>
-            {hasCloseButton && <button className={cn('close')} type="button" onClick={onClose}>
-                <CancelIcon className={cn('close-icon')} />
-            </button>}
+            {hasCloseButton && (
+                <button className={cn('close')} type="button" onClick={onClose}>
+                    <CancelIcon className={cn('close-icon')} />
+                </button>
+            )}
         </Tile>
     );
 };
 
+Notification.propTypes = {
+    type: PropTypes.oneOf(Object.values(NotificationTypes)),
+    className: PropTypes.string,
+    shadowLevel: PropTypes.oneOf(Object.values(ShadowTypes)),
+    isColored: PropTypes.bool,
+    hasCloseButton: PropTypes.bool,
+    title: PropTypes.string,
+    content: PropTypes.string,
+    link: PropTypes.string,
+    rel: PropTypes.string,
+    href: PropTypes.string,
+    target: PropTypes.oneOf(['_self', '_blank', '_parent', '_top']),
+    icon: PropTypes.element.isRequired,
+    onClose: PropTypes.func,
+    onLinkClick: PropTypes.func,
+};
 export default Notification;
--- packages/ui-core/src/components/Notification/doc/Notification.docz.tsx
+++ packages/ui-core/src/components/Notification/doc/Notification.docz.tsx
@@ -1,3 +1,40 @@
-export const marginBottom = {
-    marginBottom: '20px',
+import React, { useState } from 'react';
+
+export const wrapperStyle = {
+    display: 'grid',
+    gap: '20px',
+    gridTemplateColumns: 'repeat(auto-fit,minmax(49%, 1fr))',
+    justifyContent: 'center',
+    padding: '20px',
+};
+
+interface IDemoNotificationWrapperProps {
+    children: (prop: {
+        onClose?: () => void;
+        onLinkClick?: () => void;
+        initialCloseClickAmount: number;
+        initialLinkClickAmount: number;
+    }) => JSX.Element;
+    initialCloseClickAmount: number;
+    initialLinkClickAmount: number;
+}
+
+export const DemoNotificationWrapper: React.FC<IDemoNotificationWrapperProps> = ( {
+    initialCloseClickAmount = 0,
+    initialLinkClickAmount = 0,
+    children,
+} ) => {
+    const [closeClickAmount, setCloseClickAmount] = useState(initialCloseClickAmount);
+    const [linkClickAmount, setLinkClickAmount] = useState(initialLinkClickAmount);
+
+    return (
+        <div>
+            {children({
+                onLinkClick: () => setLinkClickAmount(linkClickAmount + 1),
+                onClose: () => setCloseClickAmount(closeClickAmount + 1),
+                initialLinkClickAmount: linkClickAmount,
+                initialCloseClickAmount: closeClickAmount,
+            })}
+        </div>
+    );
 };
