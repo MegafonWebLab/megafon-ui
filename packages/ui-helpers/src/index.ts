920b13d232aa107e974b742a1bc11c0e49d4d955
Светогор Малюгин <msvetogor@gmail.com>
2021-09-24 15:35:32 +0300

    fix(ui-helpers): eslint and prettier fixes

--- packages/ui-helpers/src/index.ts
+++ packages/ui-helpers/src/index.ts
@@ -1,8 +1,5 @@
 export { default as cnCreate } from './cnCreate';
-export { default as convert } from './convert';
+export { default as convert, ConvertTransformConfig } from './convert';
 export { default as detectTouch } from './detectTouch';
-export { default as filterDataAttrs } from './filterDataAttrs';
+export { default as filterDataAttrs, IFilterDataAttrs } from './filterDataAttrs';
 export { default as breakpoints } from './breakpoints';
-
-export type { IFilterDataAttrs } from './filterDataAttrs';
-export type { ConvertTransformConfig } from './convert';
