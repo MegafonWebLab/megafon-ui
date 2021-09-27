0e9d17090780eaa2f916c21ae90b5c7e2355ba71
Светогор Малюгин <msvetogor@gmail.com>
2021-09-27 10:32:06 +0300

    fix(ui-helpers): fix export types

--- packages/ui-helpers/src/index.ts
+++ packages/ui-helpers/src/index.ts
@@ -1,5 +1,8 @@
 export { default as cnCreate } from './cnCreate';
-export { default as convert, ConvertTransformConfig } from './convert';
+export { default as convert } from './convert';
 export { default as detectTouch } from './detectTouch';
-export { default as filterDataAttrs, IFilterDataAttrs } from './filterDataAttrs';
+export { default as filterDataAttrs } from './filterDataAttrs';
 export { default as breakpoints } from './breakpoints';
+
+export type { ConvertTransformConfig } from './convert';
+export type { IFilterDataAttrs } from './filterDataAttrs';
