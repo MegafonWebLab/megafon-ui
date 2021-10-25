e1260e836687c600cebb45da5e260baaaeeeaab3
dmitriy.lobasov <dmitriy.lobasov@shtormtech.ru>
2021-10-25 15:38:04 +0300

    refactor: fix after review

--- src/in-development/script.js
+++ src/in-development/script.js
@@ -1,29 +1,32 @@
 const cat = document.querySelector('.interesting-dialog__cat');
-const catButton = cat?.querySelector('.button');
+const catButton = cat?.querySelector('button');
 
-const FIRST_CLICK_CLASS = 'interesting-dialog__cat--first-click';
-const SECOND_CLICK_CLASS = 'interesting-dialog__cat--second-click';
-const FINALLY_CLICK_CLASS = 'interesting-dialog__cat--finally';
+const FIRST_CLICK_CLASS = 'interesting-dialog__cat_first-click';
+const SECOND_CLICK_CLASS = 'interesting-dialog__cat_second-click';
+const FINALLY_CLICK_CLASS = 'interesting-dialog__cat_finally';
 
-const handleCatbuttonClick = (e) => {
+const handleCatButtonClick = (e) => {
     e.preventDefault();
 
     if (cat?.classList.contains(FINALLY_CLICK_CLASS)) {
-        catButton?.removeEventListener('click', handleCatbuttonClick);
+        catButton?.removeEventListener('click', handleCatButtonClick);
+
         return;
     }
 
     if (cat?.classList.contains(SECOND_CLICK_CLASS)) {
         cat?.classList.add(FINALLY_CLICK_CLASS);
+
         return;
     }
 
     if (cat?.classList.contains(FIRST_CLICK_CLASS)) {
         cat?.classList.add(SECOND_CLICK_CLASS);
+
         return;
     }
 
     cat?.classList.add(FIRST_CLICK_CLASS);
 };
 
-catButton?.addEventListener('click', handleCatbuttonClick);
+catButton?.addEventListener('click', handleCatButtonClick);
