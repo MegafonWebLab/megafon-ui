13bc49b8ba3dea4eed47064f19ea61eab33822dc
dmitriy.lobasov <dmitriy.lobasov@shtormtech.ru>
2021-10-25 12:29:46 +0300

    docs: add styles and script for main page

---
+++ src/in-development/script.js
@@ -0,0 +1,29 @@
+const cat = document.querySelector('.interesting-dialog__cat');
+const catButton = cat?.querySelector('.button');
+
+const FIRST_CLICK_CLASS = 'interesting-dialog__cat--first-click';
+const SECOND_CLICK_CLASS = 'interesting-dialog__cat--second-click';
+const FINALLY_CLICK_CLASS = 'interesting-dialog__cat--finally';
+
+const handleCatbuttonClick = (e) => {
+    e.preventDefault();
+
+    if (cat?.classList.contains(FINALLY_CLICK_CLASS)) {
+        catButton?.removeEventListener('click', handleCatbuttonClick);
+        return;
+    }
+
+    if (cat?.classList.contains(SECOND_CLICK_CLASS)) {
+        cat?.classList.add(FINALLY_CLICK_CLASS);
+        return;
+    }
+
+    if (cat?.classList.contains(FIRST_CLICK_CLASS)) {
+        cat?.classList.add(SECOND_CLICK_CLASS);
+        return;
+    }
+
+    cat?.classList.add(FIRST_CLICK_CLASS);
+};
+
+catButton?.addEventListener('click', handleCatbuttonClick);
