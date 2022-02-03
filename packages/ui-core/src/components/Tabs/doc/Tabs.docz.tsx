bc20c76f497d88da19ea03cfc66a0c38a7559698
a.bashmakov <artur.bashmakov@shtormtech.ru>
2022-02-03 14:35:49 +0300

    feat(tabs): add prop outerObserveContainer for observing sticky mode from outside

--- packages/ui-core/src/components/Tabs/doc/Tabs.docz.tsx
+++ packages/ui-core/src/components/Tabs/doc/Tabs.docz.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Tabs from '../Tabs';
 
 export const DemoContent = ({ children }) => (
     <div
@@ -18,3 +19,39 @@ export const DemoContent = ({ children }) => (
         </div>
     </div>
 );
+
+export const OuterObserveContainer = ({ height = '200px', children }) => {
+    const rootRef = React.useRef<HTMLDivElement>(null);
+    const [observeContainer, setObserveContainer] = React.useState<HTMLDivElement | null>(null);
+
+    React.useEffect(() => {
+        setObserveContainer(rootRef.current);
+    }, []);
+
+    return (
+        <div ref={rootRef}>
+            <Tabs sticky outerObserveContainer={observeContainer}>
+                {children}
+            </Tabs>
+            <div style={{ width: '100%', height, background: '#F6F6F6' }} />
+        </div>
+    );
+};
+
+export const increaseSizeComponent = () => {
+    const tabs = document.querySelector('.oversize-height') as HTMLDivElement;
+    const actualHeight = tabs?.style.height;
+    const oversizeHeight = '1500px';
+
+    if (actualHeight === oversizeHeight) {
+        tabs.style.height = 'auto';
+        tabs.style.border = 'unset';
+        tabs.style.marginBottom = '0';
+    } else {
+        tabs.style.height = oversizeHeight;
+        tabs.style.border = '1px solid gray';
+        tabs.style.marginBottom = '20px';
+    }
+
+    window.dispatchEvent(new Event('resize'));
+};
