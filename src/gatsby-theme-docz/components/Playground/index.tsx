3d250419af3d60afafa44359d746bb7fbbe39d55
IGoR Fonin <menotoa@gmail.com>
2022-02-07 17:36:45 +0300

    docs(eslint): update code after add prettier config

--- src/gatsby-theme-docz/components/Playground/index.tsx
+++ src/gatsby-theme-docz/components/Playground/index.tsx
@@ -1,69 +1,64 @@
-import React from "react";
-import { cnCreate } from "@megafon/ui-helpers";
-import Copy from "@megafon/ui-icons/system-24-copy_24.svg";
-import copy from "copy-text-to-clipboard";
-import { PlaygroundProps } from "docz/dist/hooks/useComponents";
-import { Language } from "prism-react-renderer";
-import { LiveProvider, LiveError, LivePreview, LiveEditor } from "react-live";
-import { ThemeContext } from "../../themeContext";
-import { THEMES } from "./theme";
-import { Wrapper } from "./Wrapper";
-import "./Playground.less";
+import React from 'react';
+import { cnCreate } from '@megafon/ui-helpers';
+import Copy from '@megafon/ui-icons/system-24-copy_24.svg';
+import copy from 'copy-text-to-clipboard';
+import { PlaygroundProps } from 'docz/dist/hooks/useComponents';
+import { Language } from 'prism-react-renderer';
+import { LiveProvider, LiveError, LivePreview, LiveEditor } from 'react-live';
+import { ThemeContext } from '../../themeContext';
+import { THEMES } from './theme';
+import { Wrapper } from './Wrapper';
+import './Playground.less';
 
 const transformCode = (code: string) => {
-    if (code.startsWith("()") || code.startsWith("class")) return code;
+    if (code.startsWith('()') || code.startsWith('class')) return code;
 
     return `<React.Fragment>${code}</React.Fragment>`;
 };
 
-const cn = cnCreate("docz-playground");
+const cn = cnCreate('docz-playground');
 
-export const Playground: React.FC<PlaygroundProps & { useScoping?: boolean }> =
-    ({
-        style,
-        code,
-        scope,
-        language,
-        useScoping = false,
-        showEditor = true,
-    }) => {
-        const [scopeOnMount] = React.useState(scope);
-        const copyCode = () => copy(code);
+export const Playground: React.FC<PlaygroundProps & { useScoping?: boolean; showEditor?: boolean }> = ({
+    style,
+    code,
+    scope,
+    language,
+    useScoping = false,
+    showEditor = true,
+}) => {
+    const [scopeOnMount] = React.useState(scope);
+    const copyCode = () => copy(code);
 
-        return (
-            <ThemeContext.Consumer>
-                {({ theme }) => (
-                    <LiveProvider
-                        code={code}
-                        scope={scopeOnMount}
-                        transformCode={transformCode}
-                        language={language as Language}
-                        theme={THEMES[theme]}
-                    >
-                        <div className={cn("preview")}>
-                            <Wrapper content="preview">
-                                <LivePreview
-                                    className={cn("preview-content")}
-                                    data-testid="live-preview"
-                                    style={style}
-                                />
-                            </Wrapper>
-                        </div>
-                        {showEditor && (
-                            <Wrapper content="editor" useScoping={useScoping}>
-                                <div className={cn("editor")}>
-                                    <button className={cn("copy")} onClick={copyCode}>
-                                        <Copy className={cn("icon")} />
-                                    </button>
-                                    <LiveEditor data-testid="live-editor" />
-                                </div>
-                            </Wrapper>
-                        )}
-                        <LiveError data-testid="live-error" />
-                    </LiveProvider>
-                )}
-            </ThemeContext.Consumer>
-        );
-    };
+    return (
+        <ThemeContext.Consumer>
+            {({ theme }) => (
+                <LiveProvider
+                    code={code}
+                    scope={scopeOnMount}
+                    transformCode={transformCode}
+                    language={language as Language}
+                    theme={THEMES[theme]}
+                >
+                    <div className={cn('preview')}>
+                        <Wrapper>
+                            <LivePreview className={cn('preview-content')} data-testid="live-preview" style={style} />
+                        </Wrapper>
+                    </div>
+                    {showEditor && (
+                        <Wrapper useScoping={useScoping}>
+                            <div className={cn('editor')}>
+                                <button type="button" className={cn('copy')} onClick={copyCode}>
+                                    <Copy className={cn('icon')} />
+                                </button>
+                                <LiveEditor data-testid="live-editor" />
+                            </div>
+                        </Wrapper>
+                    )}
+                    <LiveError data-testid="live-error" />
+                </LiveProvider>
+            )}
+        </ThemeContext.Consumer>
+    );
+};
 
 export default Playground;
