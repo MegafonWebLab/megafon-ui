44ee66c3964da1111957553c75046407f789e673
jekatigr <jekatigr@yandex.ru>
2022-02-09 17:44:31 +0300

    feat(textwithicon): prop items removed, list items are now added by child components
    BREAKING CHANGE: prop items removed, list items are now added by child components

--- packages/ui-shared/src/components/TextWithIcon/TextWithIcon.tsx
+++ packages/ui-shared/src/components/TextWithIcon/TextWithIcon.tsx
@@ -2,18 +2,10 @@ import * as React from 'react';
 import { Header, Grid, GridColumn } from '@megafon/ui-core';
 import { cnCreate, filterDataAttrs } from '@megafon/ui-helpers';
 import * as PropTypes from 'prop-types';
+import { ITextWithIconItem } from './TextWithIconItem';
 import './TextWithIcon.less';
 
-export interface IItem {
-    /** Текст */
-    text: string;
-    /** Иконка */
-    icon: React.ReactNode;
-}
-
 export interface ITextWithIconProps {
-    /** Список строк с иконками */
-    items: IItem[];
     /** Заголовок */
     title?: string;
     /** Ссылка на корневой элемент */
@@ -24,10 +16,12 @@ export interface ITextWithIconProps {
     };
     /** Дополнительный класс для корневого элемента */
     className?: string;
+    /** Допустимый дочерний компонент */
+    children: React.ReactElement<ITextWithIconItem>[] | React.ReactElement<ITextWithIconItem>;
 }
 
 const cn = cnCreate('mfui-text-with-icon');
-const TextWithIcon: React.FC<ITextWithIconProps> = ({ items, title, rootRef, dataAttrs, className }) => (
+const TextWithIcon: React.FC<ITextWithIconProps> = ({ title, rootRef, dataAttrs, className, children }) => (
     <div className={cn([className])} ref={rootRef} {...filterDataAttrs(dataAttrs?.root)}>
         <Grid>
             <GridColumn {...{ mobile: '12', tablet: '7', desktop: '6', wide: '6' }}>
@@ -36,24 +30,13 @@ const TextWithIcon: React.FC<ITextWithIconProps> = ({ items, title, rootRef, dat
                         {title}
                     </Header>
                 )}
-                {items.map(({ text, icon }, i) => (
-                    <div className={cn('item')} key={i}>
-                        <div className={cn('svg-icon')}>{icon}</div>
-                        <div className={cn('text')}>{text}</div>
-                    </div>
-                ))}
+                {children}
             </GridColumn>
         </Grid>
     </div>
 );
 
 TextWithIcon.propTypes = {
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            text: PropTypes.string.isRequired,
-            icon: PropTypes.node.isRequired,
-        }).isRequired,
-    ).isRequired,
     title: PropTypes.string,
     rootRef: PropTypes.oneOfType([
         PropTypes.func,
@@ -62,6 +45,8 @@ TextWithIcon.propTypes = {
     dataAttrs: PropTypes.shape({
         root: PropTypes.objectOf(PropTypes.string.isRequired),
     }),
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.element.isRequired), PropTypes.element.isRequired])
+        .isRequired,
     className: PropTypes.string,
 };
 
