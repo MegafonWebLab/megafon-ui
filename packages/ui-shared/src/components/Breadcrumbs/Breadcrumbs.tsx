1ca158390c7c58b5ff117ae8ad16268b043d5ddb
Светогор Малюгин <msvetogor@gmail.com>
2022-02-17 16:38:14 +0300

    fix(breadcrumbs): allow to pass items as array of JSX elements

--- packages/ui-shared/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/ui-shared/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -4,11 +4,14 @@ import { cnCreate } from '@megafon/ui-helpers';
 import PropTypes from 'prop-types';
 import './Breadcrumbs.less';
 
-type ItemType = {
-    component?: React.ReactElement;
+type ItemObjectType = {
     title?: string;
     href?: string;
+    /* @deprecated */
+    component?: React.ReactElement;
 };
+
+type ItemType = JSX.Element | ItemObjectType;
 
 export const TextColor = {
     DEFAULT: 'default',
@@ -25,20 +28,40 @@ export type Props = {
     color?: TextColorType;
 };
 
+function isObjectElement(item: ItemType): item is ItemObjectType {
+    return !React.isValidElement(item);
+}
+
+function isJSXElement(item: ItemType): item is JSX.Element {
+    return React.isValidElement(item);
+}
+
 const cn = cnCreate('mfui-breadcrumbs');
 const Breadcrumbs: React.FC<Props> = ({ items, color = 'black', className, classes = {} }) => (
     <div className={cn({ color }, className)}>
-        {items.map(({ component, title, href }) => (
-            <div className={cn('item', classes.item)} key={title}>
-                {component}
+        {items.map((item: ItemType, i: number): JSX.Element | null => {
+            if (isJSXElement(item)) {
+                return (
+                    <div className={cn('item', classes.item)} key={i}>
+                        {item}
+                    </div>
+                );
+            }
 
-                {!component && (
-                    <TextLink href={href} color={color}>
-                        {component || title}
-                    </TextLink>
-                )}
-            </div>
-        ))}
+            if (isObjectElement(item)) {
+                return (
+                    <div className={cn('item', classes.item)} key={item.component ? i : item.title}>
+                        {item.component || (
+                            <TextLink href={item.href} color={color}>
+                                {item.title}
+                            </TextLink>
+                        )}
+                    </div>
+                );
+            }
+
+            return null;
+        })}
     </div>
 );
 
@@ -47,12 +70,17 @@ Breadcrumbs.propTypes = {
     classes: PropTypes.shape({
         item: PropTypes.string,
     }),
-    items: PropTypes.arrayOf(
-        PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            href: PropTypes.string,
-        }).isRequired,
-    ).isRequired,
+    items: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.element.isRequired),
+        PropTypes.arrayOf(
+            PropTypes.shape({
+                title: PropTypes.string,
+                href: PropTypes.string,
+                /* @deprecated */
+                component: PropTypes.element,
+            }).isRequired,
+        ),
+    ]).isRequired,
     color: PropTypes.oneOf(Object.values(TextColor)),
 };
 
