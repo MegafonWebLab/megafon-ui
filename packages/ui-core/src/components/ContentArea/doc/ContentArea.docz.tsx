e0b0c723a37f0e42ccb1c0d30df8ae36e9c82920
Светогор Малюгин <msvetogor@gmail.com>
2022-02-18 17:36:34 +0300

    fix(contentarea): add 'black' color prop value; deprecate 'default' color prop value; more examples

--- packages/ui-core/src/components/ContentArea/doc/ContentArea.docz.tsx
+++ packages/ui-core/src/components/ContentArea/doc/ContentArea.docz.tsx
@@ -1,3 +1,11 @@
-export const wrapperStyles = {
+export const wrapperStyles: React.CSSProperties = {
     padding: '20px 0',
+};
+
+export const blockStyle: React.CSSProperties = {
+    display: 'inline-grid',
+    gridGap: '20px',
+    gridTemplateColumns: 'repeat(auto-fill, minmax(100%, 1fr))',
+    alignItems: 'center',
+    width: '100%',
 };
