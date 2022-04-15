603bed3ce782676b22381e3d4df5dae141fb015c
Светогор Малюгин <svetogor.malyugin@megafon.ru>
2022-04-15 18:29:03 +0300

    feat(caption): add new Caption component; update .smallFont() less mixin
    add new typography component Caption for small texts
    
    BREAKING CHANGE: .smallFont() less mixin line-height rule now set to 18px instead of 16px
    either accept new rules or manually add line-height: 16px in your components

---
+++ packages/ui-core/src/components/Caption/Caption.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import { cnCreate, filterDataAttrs } from '@megafon/ui-helpers';
+import * as PropTypes from 'prop-types';
+import './Caption.less';
+
+const COLORS = {
+    INHERIT: 'inherit',
+    DEFAULT: 'default',
+    GRAY: 'gray',
+    WHITE: 'white',
+    GREEN: 'green',
+    PURPLE: 'purple',
+} as const;
+
+type ColorType = typeof COLORS[keyof typeof COLORS];
+
+export interface ICaptionProps {
+    /** Выравнивание по горизонтали */
+    align?: 'left' | 'center' | 'right';
+    /** Встраивание в широкий или узкий контейнер */
+    space?: 'wide' | 'tight';
+    /** Начертания шрифта: обычный или полужирный */
+    variant?: 'normal' | 'medium';
+    /** Вертикальный отступ */
+    hasMargin?: boolean;
+    /** Цвет текста */
+    color?: ColorType;
+    /** Дополнительный класс корневого элемента */
+    className?: string;
+    /** Дополнительные data атрибуты к внутренним элементам */
+    dataAttrs?: {
+        root?: Record<string, string>;
+    };
+}
+
+const cn = cnCreate('mfui-caption');
+const Caption: React.FC<ICaptionProps> = ({
+    align,
+    color = 'default',
+    space = 'wide',
+    variant = 'normal',
+    className,
+    hasMargin = true,
+    dataAttrs,
+    children,
+}) => (
+    <p
+        {...filterDataAttrs(dataAttrs?.root)}
+        className={cn(
+            {
+                align,
+                color,
+                space,
+                variant,
+                'has-margin': hasMargin,
+            },
+            className,
+        )}
+    >
+        {children}
+    </p>
+);
+
+Caption.propTypes = {
+    dataAttrs: PropTypes.shape({
+        root: PropTypes.objectOf(PropTypes.string.isRequired),
+    }),
+    align: PropTypes.oneOf(['left', 'center', 'right']),
+    space: PropTypes.oneOf(['wide', 'tight']),
+    variant: PropTypes.oneOf(['normal', 'medium']),
+    hasMargin: PropTypes.bool,
+    color: PropTypes.oneOf(Object.values(COLORS)),
+};
+
+export default Caption;
