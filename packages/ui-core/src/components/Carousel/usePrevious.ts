7b300eb9c6f805c3b425104a17f437b3b8a520c5
IGoR Fonin <menotoa@gmail.com>
2022-10-18 13:44:57 +0300

    fix(carousel): change navigation view after change slides count

---
+++ packages/ui-core/src/components/Carousel/usePrevious.ts
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function usePrevious<T>(value: T): T | undefined {
+    const ref = React.useRef<T>();
+
+    React.useEffect(() => {
+        ref.current = value;
+    }, [value]);
+
+    return ref.current;
+}
+
+export default usePrevious;
