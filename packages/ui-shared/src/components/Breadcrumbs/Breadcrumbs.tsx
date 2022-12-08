e1a30e8b3bd20479b7fc8c15cf86fc0fb9431571
Nikolay Ilin <nikolay.ilin@shtormtech.ru>
2022-12-08 09:58:50 +0300

    feat(breadcrumbs): add optional microdata attributes

--- packages/ui-shared/src/components/Breadcrumbs/Breadcrumbs.tsx
+++ packages/ui-shared/src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { TextLink } from '@megafon/ui-core';
 import { cnCreate, filterDataAttrs } from '@megafon/ui-helpers';
 import PropTypes from 'prop-types';
+import BreadcrumbsItem, { TextColor } from './components/BreadcrumbsItem';
 import './Breadcrumbs.less';
 
 type ItemObjectType = {
@@ -11,74 +11,91 @@ type ItemObjectType = {
 
 type ItemType = JSX.Element | ItemObjectType;
 
-export const TextColor = {
-    DEFAULT: 'default',
-    BLACK: 'black',
-    WHITE: 'white',
-} as const;
-
-type TextColorType = typeof TextColor[keyof typeof TextColor];
+export type TextColorType = typeof TextColor[keyof typeof TextColor];
 
 export type Props = {
+    /** Дополнительный класс корневого элемента */
+    className?: string;
+    /** Список элементов */
+    items: ItemType[];
+    /** Цвет */
+    color?: TextColorType;
+    /** Включить микроразметку */
+    hasMicrodata?: boolean;
+    /** Дополнительные классы для корневого и внутренних элементов */
+    classes?: { item?: string; itemTitle?: string; lastItemTitle?: string; link?: string };
+    /** Дополнительные data атрибуты к внутренним элементам */
     dataAttrs?: {
         root?: Record<string, string>;
+        item?: Record<string, string>;
+        itemInner?: Record<string, string>;
+        itemTitle?: Record<string, string>;
+        itemMeta?: Record<string, string>;
         link?: Record<string, string>;
     };
-    className?: string;
-    classes?: { item?: string };
-    items: ItemType[];
-    color?: TextColorType;
 };
 
-function isObjectElement(item: ItemType): item is ItemObjectType {
-    return !React.isValidElement(item);
-}
-
-function isJSXElement(item: ItemType): item is JSX.Element {
-    return React.isValidElement(item);
-}
+const isObjectElement = (item: ItemType): item is ItemObjectType => !React.isValidElement(item);
 
 const cn = cnCreate('mfui-breadcrumbs');
-const Breadcrumbs: React.FC<Props> = ({ items, color = 'default', className, classes = {}, dataAttrs }) => (
-    <div {...filterDataAttrs(dataAttrs?.root)} className={cn({ color }, className)}>
-        {items.map((item: ItemType, i: number): JSX.Element | null => {
-            if (isJSXElement(item)) {
-                return (
-                    <div className={cn('item', classes.item)} key={i}>
-                        {item}
-                    </div>
-                );
-            }
-
-            if (isObjectElement(item)) {
-                return (
-                    <div className={cn('item', classes.item)} key={item.title}>
-                        <TextLink
-                            href={item.href}
-                            color={color}
-                            dataAttrs={{
-                                root: dataAttrs?.link ? { ...filterDataAttrs(dataAttrs?.link, i + 1) } : undefined,
-                            }}
-                        >
-                            {item.title}
-                        </TextLink>
-                    </div>
-                );
-            }
-
-            return null;
-        })}
+const Breadcrumbs: React.FC<Props> = ({
+    items,
+    color = 'default',
+    className,
+    classes = {},
+    dataAttrs,
+    hasMicrodata = false,
+}) => (
+    <div
+        className={cn({ color }, className)}
+        {...filterDataAttrs(dataAttrs?.root)}
+        {...(hasMicrodata && { itemScope: true, itemType: 'https://schema.org/BreadcrumbList' })}
+    >
+        {items.map((item: ItemType, i: number): JSX.Element | null => (
+            <div className={cn('item', classes.item)} {...filterDataAttrs(dataAttrs?.item, i + 1)} key={i}>
+                {isObjectElement(item) ? (
+                    <BreadcrumbsItem
+                        title={item.title}
+                        href={item.href}
+                        color={color}
+                        index={i}
+                        isLastItem={i === items.length - 1}
+                        hasMicrodata={hasMicrodata}
+                        dataAttrs={{
+                            root: dataAttrs?.itemInner,
+                            link: dataAttrs?.link,
+                            itemTitle: dataAttrs?.itemTitle,
+                            meta: dataAttrs?.itemMeta,
+                        }}
+                        classes={{
+                            link: classes?.link,
+                            itemTitle: classes?.itemTitle,
+                            lastItemTitle: classes?.lastItemTitle,
+                        }}
+                    />
+                ) : (
+                    item
+                )}
+            </div>
+        ))}
     </div>
 );
 
 Breadcrumbs.propTypes = {
     dataAttrs: PropTypes.shape({
         root: PropTypes.objectOf(PropTypes.string.isRequired),
+        item: PropTypes.objectOf(PropTypes.string.isRequired),
+        itemInner: PropTypes.objectOf(PropTypes.string.isRequired),
+        itemTitle: PropTypes.objectOf(PropTypes.string.isRequired),
+        itemMeta: PropTypes.objectOf(PropTypes.string.isRequired),
         link: PropTypes.objectOf(PropTypes.string.isRequired),
     }),
     className: PropTypes.string,
     classes: PropTypes.shape({
         item: PropTypes.string,
+        itemTitle: PropTypes.string,
+        lastItemTitle: PropTypes.string,
+        link: PropTypes.string,
     }),
     items: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.element.isRequired),
@@ -90,6 +107,7 @@ Breadcrumbs.propTypes = {
         ),
     ]).isRequired,
     color: PropTypes.oneOf(Object.values(TextColor)),
+    hasMicrodata: PropTypes.bool,
 };
 
 export default Breadcrumbs;
