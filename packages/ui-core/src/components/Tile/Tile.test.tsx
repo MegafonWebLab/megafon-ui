bb67d8be7f801559577f70947fb07608145a8796
artur.bashmakov <artur.bashmakov@shtormtech.ru>
2023-02-09 10:16:38 +0300

    test(tile): migrate tests to react-testing-library

--- packages/ui-core/src/components/Tile/Tile.test.tsx
+++ packages/ui-core/src/components/Tile/Tile.test.tsx
@@ -1,45 +1,100 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { fireEvent, render } from '@testing-library/react';
 import Tile, { ITileProps, Theme, Radius, ShadowLevel } from './Tile';
 
 const props: ITileProps = {
-    className: 'test-class',
-    href: 'test-link',
-    theme: Theme.DARK,
-    radius: Radius.ROUNDED,
-    shadowLevel: ShadowLevel.PRESSED,
-    isInteractive: true,
-    onClick: jest.fn(),
+    href: '/link',
     target: '_blank',
+    theme: Theme.DARK,
+    onClick: jest.fn(),
+    isInteractive: true,
+    radius: Radius.ROUNDED,
+    className: 'test-class',
     rel: 'noopener noreferrer',
+    shadowLevel: ShadowLevel.PRESSED,
+    dataAttrs: { root: { 'data-testid': 'root' } },
 };
 
 describe('<Tile />', () => {
-    describe('snapshots', () => {
-        it('renders component', () => {
-            const wrapper = shallow(<Tile>Some test content</Tile>);
-            expect(wrapper).toMatchSnapshot();
-        });
+    it('should render', () => {
+        const { container } = render(<Tile>Content</Tile>);
 
-        it('renders component with props', () => {
-            const wrapper = shallow(<Tile {...props}>Some test content</Tile>);
-            expect(wrapper).toMatchSnapshot();
-        });
-
-        it('it renders with data attributes', () => {
-            const wrapper = shallow(<Tile dataAttrs={{ root: { 'data-test': 'test', 'incorrect-attr': 'test' } }} />);
-            expect(wrapper).toMatchSnapshot();
-        });
+        expect(container).toMatchSnapshot();
     });
 
-    describe('handlers', () => {
-        it('calls onClick', () => {
-            const handleClick = jest.fn();
+    it('should render with className', () => {
+        const { getByTestId } = render(
+            <Tile className={props.className} dataAttrs={props.dataAttrs}>
+                Content
+            </Tile>,
+        );
 
-            const wrapper = shallow(<Tile onClick={handleClick}>Some test content</Tile>);
+        expect(getByTestId('root')).toHaveClass(props.className as string);
+    });
 
-            wrapper.simulate('click');
-            expect(handleClick).toBeCalled();
-        });
+    it('should render with props of tag <a>', () => {
+        const { getByText } = render(
+            <Tile rel={props.rel} href={props.href} target={props.target}>
+                Content
+            </Tile>,
+        );
+        const link = getByText('Content');
+
+        expect(link).toMatchSnapshot();
+        expect(link).toHaveAttribute('rel', props.rel);
+        expect(link).toHaveAttribute('href', props.href);
+        expect(link).toHaveAttribute('target', props.target);
+    });
+
+    it('should render with theme', () => {
+        const { getByTestId } = render(
+            <Tile theme={props.theme} dataAttrs={props.dataAttrs}>
+                Content
+            </Tile>,
+        );
+
+        expect(getByTestId('root')).toHaveClass('mfui-tile_theme_dark');
+    });
+
+    it('should render with radius', () => {
+        const { getByTestId } = render(
+            <Tile radius={props.radius} dataAttrs={props.dataAttrs}>
+                Content
+            </Tile>,
+        );
+
+        expect(getByTestId('root')).toHaveClass('mfui-tile_radius_rounded');
+    });
+
+    it('should render with isInteractive', () => {
+        const { getByTestId } = render(
+            <Tile isInteractive={props.isInteractive} dataAttrs={props.dataAttrs}>
+                Content
+            </Tile>,
+        );
+
+        expect(getByTestId('root')).toHaveClass('mfui-tile_interactive');
+    });
+
+    it('should render with shadowLevel', () => {
+        const { getByTestId } = render(
+            <Tile shadowLevel={props.shadowLevel} dataAttrs={props.dataAttrs}>
+                Content
+            </Tile>,
+        );
+
+        expect(getByTestId('root')).toHaveClass('mfui-tile_shadow_pressed');
+    });
+
+    it('should call onClick', () => {
+        const mockOnClick = jest.fn();
+        const { getByTestId } = render(
+            <Tile onClick={mockOnClick} dataAttrs={props.dataAttrs}>
+                Content
+            </Tile>,
+        );
+
+        fireEvent.click(getByTestId('root'));
+        expect(mockOnClick).toBeCalled();
     });
 });
