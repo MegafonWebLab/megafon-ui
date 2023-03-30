5f40360509e703a8fab2f4936aa109a9b63bafcd
dmitriy.lobasov <dmitriy.lobasov@shtormtech.ru>
2023-03-30 12:49:14 +0300

    fix(pricebadge): new props size and subtitle

--- packages/ui-core/src/components/Badges/components/PriceBadge/PriceBadge.tsx
+++ packages/ui-core/src/components/Badges/components/PriceBadge/PriceBadge.tsx
@@ -4,6 +4,10 @@ import AttentionIcon from '@megafon/ui-icons/system-16-attention_16.svg';
 import CheckIcon from '@megafon/ui-icons/system-16-check_invert_16.svg';
 import PriceIcon from '@megafon/ui-icons/system-16-price_16.svg';
 import TimerIcon from '@megafon/ui-icons/system-16-timer_16.svg';
+import AttentionIconBig from '@megafon/ui-icons/system-24-attention_24.svg';
+import CheckIconBig from '@megafon/ui-icons/system-24-check_invert_24.svg';
+import PriceIconBig from '@megafon/ui-icons/system-24-price_24.svg';
+import TimerIconBig from '@megafon/ui-icons/system-24-timer_24.svg';
 import * as PropTypes from 'prop-types';
 import './PriceBadge.less';
 
@@ -21,18 +25,32 @@ export const PriceBadgeIcon = {
     ATTENTION: 'attention',
 } as const;
 
+export const PriseBadgeSize = {
+    SMALL: 'small',
+    BIG: 'big',
+} as const;
+
 type PriceBadgeThemeType = typeof PriceBadgeTheme[keyof typeof PriceBadgeTheme];
 type PriceBadgeIconType = typeof PriceBadgeIcon[keyof typeof PriceBadgeIcon];
+type PriseBadgeSizeType = typeof PriseBadgeSize[keyof typeof PriseBadgeSize];
 
-const getPriceBadgeIcon = (iconType: PriceBadgeIconType) => {
-    switch (iconType) {
-        case PriceBadgeIcon.TIMER:
-            return TimerIcon;
-        case PriceBadgeIcon.PRICE:
+const getPriceBadgeIcon = (iconType: PriceBadgeIconType, size: PriseBadgeSizeType) => {
+    const isBigIcon = size === PriseBadgeSize.BIG;
+
+    switch (true) {
+        case iconType === PriceBadgeIcon.TIMER && isBigIcon:
+            return TimerIconBig;
+        case iconType === PriceBadgeIcon.PRICE && isBigIcon:
+            return PriceIconBig;
+        case iconType === PriceBadgeIcon.CHECK && isBigIcon:
+            return CheckIconBig;
+        case iconType === PriceBadgeIcon.ATTENTION && isBigIcon:
+            return AttentionIconBig;
+        case iconType === PriceBadgeIcon.PRICE:
             return PriceIcon;
-        case PriceBadgeIcon.CHECK:
+        case iconType === PriceBadgeIcon.CHECK:
             return CheckIcon;
-        case PriceBadgeIcon.ATTENTION:
+        case iconType === PriceBadgeIcon.ATTENTION:
             return AttentionIcon;
         default:
             return TimerIcon;
@@ -48,6 +66,10 @@ export interface IPriceBadgeProps {
     theme?: PriceBadgeThemeType;
     /** Дополнительный класс корневого элемента */
     className?: string;
+    /** Размер бейджа */
+    size?: PriseBadgeSizeType;
+    /** Дополнительный текст (только для size="big") */
+    subTitle?: string;
     /** Дополнительные data-атрибуты к внутренним элементам */
     dataAttrs?: {
         root?: Record<string, string>;
@@ -60,18 +82,24 @@ const PriceBadge: React.FC<IPriceBadgeProps> = ({
     iconType = 'timer',
     isAdaptive = false,
     theme = 'grey',
+    size = 'small',
+    subTitle,
     className,
     dataAttrs,
     children,
 }) => {
-    const Icon = getPriceBadgeIcon(iconType);
+    const Icon = getPriceBadgeIcon(iconType, size);
+    const showSubTitle = size === 'big' && !!subTitle;
 
     return (
-        <div {...filterDataAttrs(dataAttrs?.root)} className={cn({ theme, adaptive: isAdaptive }, className)}>
+        <div {...filterDataAttrs(dataAttrs?.root)} className={cn({ theme, adaptive: isAdaptive, size }, className)}>
             <div className={cn('icon-container')}>
                 <Icon className={cn('icon')} />
             </div>
-            <span className={cn('text')}>{children}</span>
+            <div className={cn('text')}>
+                <div>{children}</div>
+                {showSubTitle && <div className={cn('sub-title')}>{subTitle}</div>}
+            </div>
         </div>
     );
 };
@@ -80,6 +108,8 @@ PriceBadge.propTypes = {
     isAdaptive: PropTypes.bool,
     iconType: PropTypes.oneOf(Object.values(PriceBadgeIcon)),
     theme: PropTypes.oneOf(Object.values(PriceBadgeTheme)),
+    size: PropTypes.oneOf(Object.values(PriseBadgeSize)),
+    subTitle: PropTypes.string,
     className: PropTypes.string,
     dataAttrs: PropTypes.shape({
         root: PropTypes.objectOf(PropTypes.string.isRequired).isRequired,
